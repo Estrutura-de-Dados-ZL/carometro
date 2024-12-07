2a01b2b0ca2578dd97a5d7341f0167d8eafb27e2
deArrudal <arrudalucas@hotmail.com>
2024-12-06 21:17:59 -0300

    add new users

--- src/main/java/com/carometro/config/DataLoader.java
+++ src/main/java/com/carometro/config/DataLoader.java
@@ -2,6 +2,7 @@ package com.carometro.config;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,11 +10,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.carometro.model.Aluno;
 import com.carometro.model.Curso;
 import com.carometro.model.Role;
+import com.carometro.model.Turma;
+import com.carometro.model.TurmaId;
 import com.carometro.model.Usuario;
+import com.carometro.repository.AlunoRepository;
 import com.carometro.repository.CursoRepository;
 import com.carometro.repository.RoleRepository;
+import com.carometro.repository.TurmaRepository;
 import com.carometro.repository.UsuarioRepository;
 
 import lombok.AllArgsConstructor;
@@ -30,6 +36,12 @@ public class DataLoader implements CommandLineRunner {
 
     @Autowired
     private CursoRepository cursoRepository;
+
+    @Autowired
+    private TurmaRepository turmaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -60,6 +72,55 @@ public class DataLoader implements CommandLineRunner {
         for (String curso : cursos) {
             criarCurso(curso);
         }
+
+        // Criar turmas
+        String ano = "2024";
+        String semestre = "1";
+        criarTurma(ano, semestre);
+
+        // Criar alunos
+        int[] rolesAlunoId = { 2 };
+        String[][] alunos = {
+                { "jane.doe@example.com", "Jane Doe", "senha123", "Campo livre 1", "Comentário 1",
+                        "https://i.pinimg.com/236x/19/bd/eb/19bdeb93ad73ce5ead4800d254c51008.jpg",
+                        "https://jane-link.com", "RA12345", "1", "2024", "1" },
+                { "mary.smith@example.com", "Mary Smith", "senha456", "Campo livre 2", "Comentário 2",
+                        "https://i.pinimg.com/236x/19/bd/eb/19bdeb93ad73ce5ead4800d254c51008.jpg",
+                        "https://mary-link.com", "RA12346", "2", "2024", "1" },
+                { "lisa.jones@example.com", "Lisa Jones", "senha789", "Campo livre 3", "Comentário 3",
+                        "https://i.pinimg.com/236x/19/bd/eb/19bdeb93ad73ce5ead4800d254c51008.jpg",
+                        "https://lisa-link.com", "RA12347", "3", "2024", "1" },
+                { "susan.brown@example.com", "Susan Brown", "senha101", "Campo livre 4", "Comentário 4",
+                        "https://i.pinimg.com/236x/19/bd/eb/19bdeb93ad73ce5ead4800d254c51008.jpg",
+                        "https://susan-link.com", "RA12348", "4", "2024", "1" },
+                { "emily.johnson@example.com", "Emily Johnson", "senha202", "Campo livre 5", "Comentário 5",
+                        "https://i.pinimg.com/236x/19/bd/eb/19bdeb93ad73ce5ead4800d254c51008.jpg",
+                        "https://emily-link.com", "RA12349", "5", "2024", "1" },
+                { "anna.martin@example.com", "Anna Martin", "senha303", "Campo livre 6", "Comentário 6",
+                        "https://i.pinimg.com/236x/19/bd/eb/19bdeb93ad73ce5ead4800d254c51008.jpg",
+                        "https://anna-link.com", "RA12350", "6", "2024", "1" },
+                { "john.doe@example.com", "John Doe", "senha123", "Campo livre 1", "Comentário 1",
+                        "https://img.freepik.com/fotos-gratis/feche-o-retrato-de-um-rosto-jovem-barbudo_171337-2887.jpg?semt=ais_hybrid",
+                        "https://john-link.com", "RA22345", "7", "2024", "1" },
+                { "mark.smith@example.com", "Mark Smith", "senha456", "Campo livre 2", "Comentário 2",
+                        "https://img.freepik.com/fotos-gratis/feche-o-retrato-de-um-rosto-jovem-barbudo_171337-2887.jpg?semt=ais_hybrid",
+                        "https://mark-link.com", "RA22346", "8", "2024", "1" },
+                { "paul.jones@example.com", "Paul Jones", "senha789", "Campo livre 3", "Comentário 3",
+                        "https://img.freepik.com/fotos-gratis/feche-o-retrato-de-um-rosto-jovem-barbudo_171337-2887.jpg?semt=ais_hybrid",
+                        "https://paul-link.com", "RA22347", "9", "2024", "1" },
+                { "david.brown@example.com", "David Brown", "senha101", "Campo livre 4", "Comentário 4",
+                        "https://img.freepik.com/fotos-gratis/feche-o-retrato-de-um-rosto-jovem-barbudo_171337-2887.jpg?semt=ais_hybrid",
+                        "https://david-link.com", "RA22348", "1", "2024", "2" },
+                { "james.johnson@example.com", "James Johnson", "senha202", "Campo livre 5", "Comentário 5",
+                        "https://img.freepik.com/fotos-gratis/feche-o-retrato-de-um-rosto-jovem-barbudo_171337-2887.jpg?semt=ais_hybrid",
+                        "https://james-link.com", "RA22349", "2", "2024", "2" },
+                { "michael.martin@example.com", "Michael Martin", "senha303", "Campo livre 6", "Comentário 6",
+                        "https://img.freepik.com/fotos-gratis/feche-o-retrato-de-um-rosto-jovem-barbudo_171337-2887.jpg?semt=ais_hybrid",
+                        "https://michael-link.com", "RA22350", "3", "2024", "2" }
+        };
+        for (String[] aluno : alunos) {
+            atualizarAluno(aluno, rolesAlunoId);
+        }
     }
 
     private void criarRole(String roleName) {
@@ -72,16 +133,16 @@ public class DataLoader implements CommandLineRunner {
 
     private void criarUsuario(String email, String password, int[] rolesId) {
         if (!usuarioRepository.existsByEmail(email)) {
-            Usuario administrador = new Usuario();
-            administrador.setEmail(email);
-            administrador.setSenha(passwordEncoder.encode(password));
+            Usuario usuario = new Usuario();
+            usuario.setEmail(email);
+            usuario.setSenha(passwordEncoder.encode(password));
             List<Role> roles = new ArrayList<>();
             for (int roleId : rolesId) {
                 Role role = roleRepository.getReferenceById(roleId);
                 roles.add(role);
             }
-            administrador.setRoles(roles);
-            usuarioRepository.save(administrador);
+            usuario.setRoles(roles);
+            usuarioRepository.save(usuario);
         }
     }
 
@@ -90,6 +151,48 @@ public class DataLoader implements CommandLineRunner {
             Curso cursoNovo = new Curso();
             cursoNovo.setNome(cursoNome);
             cursoRepository.save(cursoNovo);
+        }
+    }
+
+    private void criarTurma(String ano, String semestre) {
+        List<Curso> cursos = cursoRepository.findAll();
+        for (Curso curso : cursos) {
+            TurmaId turmaId = new TurmaId(curso.getId(), ano, semestre);
+            if (!turmaRepository.existsById(turmaId)) {
+                Turma turma = new Turma(turmaId, curso);
+                turmaRepository.save(turma);
+            }
+        }
+    }
+
+    private void atualizarAluno(String[] alunoData, int[] rolesId) {
+        if (!usuarioRepository.existsByEmail(alunoData[0])) {
+            Aluno aluno = new Aluno();
+            aluno.setEmail(alunoData[0]);
+            aluno.setSenha(passwordEncoder.encode(alunoData[2]));
+            aluno.setNome(alunoData[1]);
+            List<Role> roles = new ArrayList<>();
+            for (int roleId : rolesId) {
+                Role role = roleRepository.getReferenceById(roleId);
+                roles.add(role);
+            }
+            aluno.setRoles(roles);
+
+            aluno.setCampoLivre(alunoData[3]);
+            aluno.setComentario(alunoData[4]);
+            aluno.setFoto(alunoData[5]);
+            aluno.setLink(alunoData[6]);
+            aluno.setRa(alunoData[7]);
+            aluno.setPendente(false);
+            aluno.setPermissaoDados(true);
+
+            // aloca turma
+            TurmaId turmaId = new TurmaId(Long.parseLong(alunoData[8]), alunoData[9], alunoData[10]);
+            Optional<Turma> turma = turmaRepository.findById(turmaId);
+            if (turma.isPresent())
+                aluno.setTurma(turma.get());
+
+            alunoRepository.save(aluno);
         }
     }
 }
